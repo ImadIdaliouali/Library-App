bbb44ba3fb19877922ac5cc275a5aac8677d0b88
imad <imadidaliouali@gmail.com>
2022-12-21 10:41:37 +0100

    Login interface done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import view.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Login();
     }
 }
---
+++ src/view/Register.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class Register extends JFrame {
+    public Register() {
+        this.setTitle("Register");
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
